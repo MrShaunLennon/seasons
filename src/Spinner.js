87cb6dfd8c9e1d5f24cd8eefda43d74d8b227be7
Shaun Lennon <shaunlennon1992@gmail.com>
2020-10-16 16:33:13 +0100

    props passed in

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,10 +1,11 @@
+import { POINT_CONVERSION_COMPRESSED } from 'constants';
 import React from 'react';
 
-const Spinner = () => {
+const Spinner = (props) => {
   return (
     <div className="ui active dimmer">
       <div className="ui big text loader">
-        Loading...
+        {props.message}
       </div>
     </div>
   );
