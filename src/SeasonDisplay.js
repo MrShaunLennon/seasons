5063adba8e27749941696502adc34749fe081241
Shaun Lennon <shaunlennon1992@gmail.com>
2020-10-16 13:11:14 +0100

    Refactored

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -4,16 +4,19 @@ const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? 'Summer' : 'Winter';
   } else {
-    return lat < 0 ? 'Winter' : 'Summer';
+    return lat > 0 ? 'Winter' : 'Summer';
   }
 };
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
+  const text = season === 'Winter' ? 'Burr, it is chilly!' : 'Lets hit the beach!'
 
-console.log(season);
-
-  return <div>Season Display</div>;
-}
+  return (
+    <div>
+      {text}
+    </div>
+  );
+};
 
 export default SeasonDisplay;
